f407ce2ff5b37cae53e0b778ed47d11126baa94d
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-08-24 18:15:13 +0530

    setup project

--- src/App.js
+++ src/App.js
@@ -15,10 +15,6 @@ class App extends Component {
       <div className='App'>
         <ul className='list-group list-group-flush'>
           <li className='list-group-item'>Cras justo odio</li>
-          <li className='list-group-item'>Dapibus ac facilisis in</li>
-          <li className='list-group-item'>Morbi leo risus</li>
-          <li className='list-group-item'>Porta ac consectetur ac</li>
-          <li className='list-group-item'>Vestibulum at eros</li>
         </ul>
       </div>
     )
